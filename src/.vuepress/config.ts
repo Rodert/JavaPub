df6b863d33e0e13f2ed97f329250fbaab7895fb2
javapub <731444260@qq.com>
2025-07-15 07:56:18 +0800

    feat: path

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/JavaPub/",
 
   lang: "zh-CN",
   title: "JavaPub",
