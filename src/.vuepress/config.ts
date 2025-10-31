6404963d9756dd09cbfc76643ede0231dfa5c9fd
javapub <iswangshiyu@foxmail.com>
2025-10-31 18:27:36 +0800

    feat: router

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/JavaPub/",
+  base: "/",
 
   lang: "zh-CN",
   title: "JavaPub",
